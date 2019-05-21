8224b6c68c04278e0816c46b9ea8307eb2a3ffa2
Anuraag Basu <anuraag.basu@100grams.nl>
2019-05-21 12:10:34 +0200

    added an example of method chaining

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,20 @@ import SearchCarousel from "./components/SearchCarousel";
 
 import "./css/index.css";
 
-const root = document.getElementById("root");
+const [firstSection, secondSection] = document.querySelectorAll(".section");
 const searchCarousel = new SearchCarousel();
-root && root.appendChild(searchCarousel);
+secondSection && secondSection.appendChild(searchCarousel);
+
+const codeHolder = document.createElement("code");
+codeHolder.innerHTML = `
+	const addN = (firstNumber: number) => {<br />
+	  	return (secondNumber: number) => {<br />
+			return firstNumber + secondNumber;<br />
+		};<br />
+	};<br />
+	<br />
+	<br />
+	const addEight = addN(8);<br />
+	console.log(addEight(100));<br />
+`;
+firstSection.appendChild(codeHolder);
